bf09c4d309c9120a9dcd1d4b177bd15cfb72546b
bhavadharanijar <36373014+bhavadharanijar@users.noreply.github.com>
2018-03-20 17:27:16 +0530

    Create asd.c

---
+++ asd.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int j,a1[50],t,n,flag;
+for(t=0;t<n;t++);
+{
+flag=0;
+for(j=0;j<n;j++)
+{
+if(a1[t]==a1[j])
+{
+flag++;
+}
+else
+continue:
+}
+if(flag==1)
+printf("%d",a1[t]);
+}
+return 0;
+}
